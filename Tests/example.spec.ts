2e69a1c02d1d4b9fe37cc4985f191759f4558388
Slavik <slavikck89@gmail.com>
2025-05-02 18:34:29 +0300

    Homework #14

--- Tests/example.spec.ts
+++ Tests/example.spec.ts
@@ -1,5 +1,6 @@
-import { test, expect } from '@playwright/test';
+//import { test, expect } from '@playwright/test';
 
+/*
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
@@ -16,3 +17,43 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
+//   // Expect a title "to contain" a substring.
+//   await expect(page).toHaveTitle(/Playwright/);
+// });
+*/
+
+
+/*
+//Lesson 14
+test('has title', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  const userNameField = page.locator('input[data-test="username"]');
+  const passwordField = page.locator('//input[@name="password"]');
+  const loginButton = page.locator('input[name="login-button"]');
+  
+  await userNameField.fill('test_user');
+  await passwordField.fill('testpassword');
+
+  await page.locator('#login-button').click();
+  //await expect(page).toHaveURL('https://www.saucedemo.com/')
+
+  //const element = page.locator('div', {has: page.locator('input[data-test="username"')});
+  
+  //const loginButton = page.getByTestId('input[name="login-button"]');
+  //const loginButton = page.getByLabel('Login').click
+
+  await expect(userNameField,).toBeVisible();
+  await expect(passwordField).toBeVisible();
+  await expect(loginButton).toBeVisible();
+
+});
+
+test ('checkbox check and uncheck', async ({page}) => {
+  await page.goto('https://practice.expandtesting.com/checkboxes');
+  
+  await page.getByLabel('Checkbox 2').uncheck();
+  await page.getByLabel('Checkbox 2').check();
+
+});
+*/
---
+++ Tests/login.spec.ts
@@ -0,0 +1,100 @@
+import { test, expect } from '@playwright/test';
+
+const userData = {
+    valid: { username: 'standard_user', password: 'secret_sauce' },
+    lockedOut: { username: 'locked_out_user', password: 'secret_sauce' },
+    problem: { username: 'problem_user', password: 'secret_sauce' },
+    performance: { username: 'performance_glitch_user', password: 'secret_sauce' },
+    invalid: { username: 'invalid_user', password: 'wrong_pass' }
+  };
+
+test.describe('Login Page Tests', () => {
+
+    test.beforeEach(async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/');
+    
+});
+
+    test('Loading Login page and checking title', async ({ page }) => {
+    
+        
+        const title = page.locator('//div[@class="login_logo"]');
+        await expect(title).toContainText('Swag Labs');
+
+    });
+
+    test('Checking Login form fields', async ({ page }) => {
+        
+        const userName = page.locator('input[data-test="username"]');
+        const userPassword = page.locator('input[data-test="password"]');
+        const loginButton = page.locator('input[data-test="login-button"]');
+
+        await expect(userName).toBeVisible();
+        await expect(userPassword).toBeVisible();
+        await expect(loginButton).toBeVisible();
+
+    });
+
+    test('Log in without username', async ({ page }) => {
+    
+        await page.fill('input[data-test="password"]', userData.valid.password);
+        await page.click('input[data-test="login-button"]');
+
+        const errorUsername = await page.locator('//*[@id="login_button_container"]//h3');
+        await expect(errorUsername).toContainText('Epic sadface: Username is required')
+        await page.click('//button[@class="error-button"]');
+    
+    });
+
+    test('Log in without password', async ({ page }) => {
+
+        await page.fill('input[data-test="username"]', userData.valid.username);
+        await page.click('input[data-test="login-button"]');
+
+        const errorPassword = await page.locator('//*[@id="login_button_container"]//h3');
+        await expect(errorPassword).toContainText('Epic sadface: Password is required');
+        await page.click('//button[@class="error-button"]');
+    
+    });
+
+    test('Log in with invalid username/password', async ({ page }) => {
+    
+        await page.fill('input[data-test="username"]', 'test');
+        await page.fill('input[data-test="password"]', userData.valid.password);
+        await page.click('input[data-test="login-button"]');
+
+        const invalidUsernamePassword = await page.locator('//*[@id="login_button_container"]//h3');
+        await expect(invalidUsernamePassword).toContainText('Epic sadface: Username and password do not match any user in this service');
+        await page.click('//button[@class="error-button"]');
+
+    });
+
+    test('Log in with locked user', async ({ page }) => {
+
+        await page.fill('input[data-test="username"]', userData.lockedOut.username), { delay: 150 };
+        await page.fill('input[data-test="password"]', userData.lockedOut.password);
+        await page.click('input[data-test="login-button"]');
+
+        const errorLocked = page.locator('//*[@id="login_button_container"]//h3');
+        await expect(errorLocked).toContainText('Epic sadface: Sorry, this user has been locked out.');
+        await page.click('//button[@class="error-button"]');
+
+    });
+
+    test('Log in with valid user', async ({ page }) => {
+
+        await page.fill('input[data-test="username"]', userData.valid.username), { delay: 150 };
+        await page.fill('input[data-test="password"]', userData.valid.password);
+        await page.click('input[data-test="login-button"]');
+
+        await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+        const pageTitle = page.locator('span[data-test="title"]');
+
+        await expect(pageTitle).toContainText('Products');
+        await page.click('//button[@id="react-burger-menu-btn"]');
+        await page.click('//a[contains(text(), "Logout")]');
+        await expect(page).toHaveURL('https://www.saucedemo.com/');
+
+    });
+
+});
